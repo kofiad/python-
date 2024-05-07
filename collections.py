af06feb9f41d5521f8a2e44c00c9da070e14f7c6
Gilbert A. Henyo <86112921+kofiad@users.noreply.github.com>
2024-05-07 21:03:19 +0000

    Create collections.py

---
+++ collections.py
@@ -0,0 +1,20 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange"
+print(myFruitList)
+myFruitTuple = ["apple", "banana", "pineapple"]
+print(myFruitTuple)
+print(type(myFruitTuple))
+print(myFruitTuple[0])
+print(myFruitTuple[1])
+print(myFruitTuple[2])
+myFavouriteFruitDictionary = {"Akua" : "apple", "Saanvi" : "banana", "Paulo" : "pineapple"}
+print(myFavouriteFruitDictionary)
+print(type(myFavouriteFruitDictionary))
+print(myFavouriteFruitDictionary["Akua"])
+print(myFavouriteFruitDictionary["Saanvi"])
+print(myFavouriteFruitDictionary["Paulo"])
