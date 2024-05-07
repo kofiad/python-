07eb794b8fd2ffa163842560e1d618ed3c143f2b
Gilbert A. Henyo <86112921+kofiad@users.noreply.github.com>
2024-05-07 20:29:47 +0000

    Create numeric_data.py

---
+++ numeric_data.py
@@ -0,0 +1,17 @@
+print("Python has three numeric data types: int, float and complex")
+my_value = 1
+print(my_value)
+print(type(my_value))
+print(str(my_value) + " is of the data type " + str(type(my_value)))
+my_value2 = 3.4
+print(my_value2)
+print(type(my_value2))
+print(str(my_value2) + " is of the data type " + str(type(my_value2)))
+my_value3 = 5j
+print(my_value3)
+print(type(my_value3))
+print(str(my_value3) + " is of the data type " + str(type(my_value3)))
+my_value4 = True
+print(my_value4)
+print(type(my_value4))
+print(str(my_value4) + " is of the data type " + str(type(my_value4)))
