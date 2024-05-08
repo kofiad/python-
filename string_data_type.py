10e4fa21807d864a2762c759c9d0fe4bd50782f4
Gilbert A. Henyo <86112921+kofiad@users.noreply.github.com>
2024-05-08 11:10:17 +0000

    Create string_data_type.py

---
+++ string_data_type.py
@@ -0,0 +1,13 @@
+myString = "This is a string"
+print(myString)
+print(type(myString))
+print(str(myString) + " is of data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("What is you name?")
+print(name)
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
